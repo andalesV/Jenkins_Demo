74634f4f08f661f44718bfe3e3e7e9f06bb1b462
Andales Victor III Garcia <andalesvictoriii.garcia@maybank.com>
2021-01-19 12:13:03 +0800

    commit file

---
+++ Hello.java
@@ -0,0 +1,8 @@
+package com.maybank.forms.controller;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, this is from Git repository!!!");
+    }
+}
